ee8df744839f71fd107edd0d5d6e95626e519848
Jose Cazarin <joseespiriki@gmail.com>
2020-03-22 17:06:13 -0600

    Initial commit

---
+++ _builds_makefile/CMakeFiles/foo.elf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foo.elf.dir/foo.cpp.obj"
+  "foo.elf"
+  "foo.elf.manifest"
+  "foo.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/foo.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ foo.cpp
@@ -0,0 +1,11 @@
+#include <math.h> 
+#include <cstdio>
+
+int main() {
+  int a = 2;
+  int b = 4;
+  
+  auto result =  pow (b, a);
+  
+  printf("oi");
+}
